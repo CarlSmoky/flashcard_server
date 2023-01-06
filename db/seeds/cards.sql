ddf8be80b950dcc07b47e1ced16f704d93e59bf9
tsumitakaoru <k.tsumita01@gmail.com>
2023-01-06 12:28:17 -0500

    Change name

--- db/seeds/cards.sql
+++ db/seeds/cards.sql
@@ -1,7 +1,8 @@
 INSERT INTO
-cards(front, back)
+cards(deck_id, term, definition)
 VALUES
-    ('bonjour!', 'hello'),
-    ('chat', 'cat'),
-    ('fromage', 'cheese'),
-    ('champignon', 'mushroom');
+    (1, 'bonjour!', 'hello'),
+    (1, 'chat', 'cat'),
+    (1, 'fromage', 'cheese'),
+    (1, 'champignon', 'mushroom'),
+    (2, 'What is Callback hell?', 'A situation where you have multiple asyndhromouns functions. Those functions depends on another, so it could get quite messy');
