0eb20e420729c9ad3633676e255fc2a1b2b5d8a3
tsumitakaoru <k.tsumita01@gmail.com>
2023-03-09 10:38:06 -0500

    Change schema

--- db/seeds/stats.sql
+++ db/seeds/stats.sql
@@ -1,5 +1,8 @@
 INSERT INTO
-stats(card_id, user_id)
+stats(card_id, user_id, learning, star, )
 VALUES
-    (1, 2),
-    (1, 3);
+    (1, 1, true, true),
+    (2, 1, true, true),
+    (3, 1, true, false),
+    (4, 1, false, false),
+    (5, 1, false, true);
